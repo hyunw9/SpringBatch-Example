f2770d92807b8b65025a5486ed24cd63266fe614
hyunw9 <rkdgusdnr32@naver.com>
2025-03-09 22:15:15 +0900

    [Feat] : Test Http 정의

---
+++ src/test/kotlin/com/mailpoc/MailpocApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.mailpoc
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MailpocApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
